99073aa1645e819fab58882c4c927ca1c1e57f71
Vasilii <goooneoooogle@gmail.com>
2020-02-10 23:39:29 +0200

    Finish

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,23 @@
 $(function() {
-	
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('.header__area').addClass('scrolled');
+    } else {
+      $('.header__area').removeClass('scrolled');
+    }
+  });
+  // Hamburger
+  var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+
+  var hamburgers = document.querySelectorAll(".hamburger");
+  if (hamburgers.length > 0) {
+    forEach(hamburgers, function(hamburger) {
+      hamburger.addEventListener("click", function() {
+        this.classList.toggle("is-active");
+      }, false);
+    });
+  }
+  $('.hamburger').on('click', function() {
+		$('.mobile--menu').slideToggle();
+  });
 });
